4db2dac947873e58387baf0935532ad179b474d7
nassim <nassimohsin@gmail.com>
2026-09-23 21:33:59 +0200

    parse bestandje, voor nu testen of het werkt zoals het moet met een handmatige accession code

---
+++ src/bpapge_parser/parser.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+from urllib.request import urlretrieve
+from Bio import SwissProt
+
+DATA_DIR = Path("data")
+OUPUT_DIR = Path("output")
+
+DATA_DIR.mkdir(exist_ok=True)
+OUPUT_DIR.mkdir(exist_ok=True)
+
+
+def download_uniprot(accession):
+    url = f"https://rest.uniprot.org/uniprotkb/{accession}.txt"
+    output_file = DATA_DIR / f"{accession}.txt"
+
+    urlretrieve(url, output_file)
+
+    print(f"{accession} is gedownload")
+
+    return output_file
+
+
+def parse_uniprot(file_path):
+    with open(file_path, encoding="utf=8") as handle:
+        record = SwissProt.read(handle)
+
+        print("UniProt accession:")
+        print(record.accessions[0])
+
+        print("\nEiwitnaam:")
+        print(record.description)
+
+        print("\nGen:")
+        print(record.gene_name)
+
+        print("\nOrganisme:")
+        print(record.organism)
+
+        print("\nAminozuursequentie:")
+        print(record.sequence)
+
+
+file = download_uniprot("M7BTR0")
+
+parse_uniprot(file)
