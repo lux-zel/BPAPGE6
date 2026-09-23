32442eb0d84ffa5305a852db09a16fb241b42f13
Yuna Woesthuis <yuna@elsive.com>
2026-09-23 12:58:59 +0200

    Gen database

--- src/database_opzetten.py
+++ src/database_opzetten.py
@@ -13,7 +13,48 @@ try:
     print('Connection open')
     cursor = conn.cursor()
 
+    def tabellen_verwijderen():
+        verwijder = [
+            "gen"
+        ]
 
+        for verwijder_tabel in verwijder:
+            cursor.execute(f"DROP TABLE IF EXISTS {verwijder_tabel} CASCADE")
+
+    # TABELLEN AANMAKEN:
+    def gen_aanmaken():
+        return """CREATE TABLE IF NOT EXISTS gen 
+        (Gen_ID VARCHAR(50) PRIMARY KEY,
+        Naam VARCHAR(50),
+        Sequentie VARCHAR(500),
+        Startpositie INT,
+        Eindpositie INT,
+        Beschrijving VARCHAR(250),
+        Strand VARCHAR(1)
+        )"""
+
+
+    if __name__ == '__main__':
+        # Alle tabellen verwijderen
+        tabellen_verwijderen()
+
+        # File inlezen
+
+        # Alle tabellen weer opnieuw aanmaken
+        tabellen = [
+            gen_aanmaken(),
+
+        ]
+
+        for tabel in tabellen:
+            cursor.execute(tabel)
+
+        # Tabel vullen:
+
+        # Connectie sluiten
+        conn.commit()
+        conn.close()
+        print('Connection closed')
 
 except psycopg2.Error as error:
     print("Error", error)
@@ -24,4 +65,3 @@ except psycopg2.Error as error:
     if conn:
         conn.close()
         print('Connection closed')
-
